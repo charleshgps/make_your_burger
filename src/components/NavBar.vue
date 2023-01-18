f2e29366ffe0425b8e3bfb34203b555efc3787bc
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2023-01-18 19:15:44 -0300

    router config

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="nav">
+        <router-link to="/">
+            <img src="" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/pedidos">Pedidos</router-link>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+ 
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+</script>
